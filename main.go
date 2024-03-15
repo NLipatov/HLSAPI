c43a60d65d2f0d2d5e7c33bbfa814a36e152a642
Nikita Lipatov <6stringsohei@gmail.com>
2024-03-15 16:38:49 +0300

    createStorageFolder function implementation

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"hlsapi/src/Configuration"
+	ConfigurationModels "hlsapi/src/Configuration/Models"
 	"hlsapi/src/FileEndpoints"
 	"hlsapi/src/SentinelServiceDaemon"
 	"net/http"
+	"os"
 )
 
 func main() {
 	Configuration.Init(fmt.Sprintf("%s", "appSettings.json"))
 	configuration := Configuration.ReadConfiguration()
+
+	createStorageFolder(configuration)
 
 	if configuration.SentinelServiceDaemonConfiguration.ShouldRun {
 		go func() {
@@ -28,5 +33,19 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+}
+
+func createStorageFolder(configuration ConfigurationModels.Configuration) {
+	_, err := os.Stat(configuration.StorageFolderPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			dirCreationError := os.Mkdir(configuration.StorageFolderPath, os.FileMode(744))
+			if dirCreationError != nil {
+				panic(dirCreationError)
+			}
+			return
+		}
+		panic(err)
 	}
 }
