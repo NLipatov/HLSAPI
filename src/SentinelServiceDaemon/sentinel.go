9fd5a5bdbb0bbea56d22bb0e9ebf0938a0ffa9cd
Nikita Lipatov <6stringsohei@gmail.com>
2024-03-15 16:19:51 +0300

    sentinel daemon implementation

---
+++ src/SentinelServiceDaemon/sentinel.go
@@ -0,0 +1,57 @@
+package SentinelServiceDaemon
+
+import (
+	"fmt"
+	"hlsapi/src/Configuration"
+	ConfigurationModels "hlsapi/src/Configuration/Models"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func Start(path string) {
+	configuration := getConfiguration()
+	if !configuration.SentinelServiceDaemonConfiguration.ShouldRun {
+		return
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log(fmt.Sprintf("Error: %s", err.Error()))
+		return
+	}
+
+	for _, entry := range entries {
+		if entry.Type().IsDir() {
+			Start(filepath.Join(path, entry.Name()))
+		} else {
+			entryPath := filepath.Join(path, entry.Name())
+			fileInfo, err := os.Stat(entryPath)
+			if err != nil {
+				log(fmt.Sprintf("Could not get file information. Filepath: %s", entryPath))
+			}
+
+			if fileInfo.ModTime().Minute() > configuration.SentinelServiceDaemonConfiguration.StorageLimitMinutes {
+				err = os.Remove(entryPath)
+				if err != nil {
+					log(fmt.Sprintf("Error on delition (%s): %s", err.Error(), entryPath))
+					continue
+				}
+				log(fmt.Sprintf("Deleted: %s", entryPath))
+			}
+		}
+	}
+
+	interval := getConfiguration().SentinelServiceDaemonConfiguration.StorageChecksIntervalMinutes
+	time.Sleep(time.Duration(interval) * time.Minute)
+	Start(configuration.StorageFolderPath)
+}
+
+func getConfiguration() ConfigurationModels.Configuration {
+	configuration := Configuration.ReadConfiguration()
+	return configuration
+}
+
+func log(message string) {
+	fmt.Println("Sentinel: ", message)
+}
