a12debb0fb1e6ea92a1ac1757295e8bb84cf48d2
Nikita Lipatov <6stringsohei@gmail.com>
2024-04-16 16:34:49 +0300

    new error type FileCantBeConvertedToM3U8

--- src/Application/Errors/ApplicationLayerErrors.go
+++ src/Application/Errors/ApplicationLayerErrors.go
@@ -6,3 +6,10 @@ type FileCantBeStored struct {
 func (e FileCantBeStored) Error() string {
 	return "File cannot be stored"
 }
+
+type FileCantBeConvertedToM3U8 struct {
+}
+
+func (e FileCantBeConvertedToM3U8) Error() string {
+	return "File cannot be converted to m3u8"
+}
