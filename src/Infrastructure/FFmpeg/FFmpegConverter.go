4364d0b23d00679bfb5e51fa3ea701e7184feda7
Nikita Lipatov <6stringsohei@gmail.com>
2024-04-24 16:05:36 +0300

    FFmpegConverter.go is now converting video to mp4 if it's not mp4, to create a proper m3u8 playlist

--- src/Infrastructure/FFmpeg/FFmpegConverter.go
+++ src/Infrastructure/FFmpeg/FFmpegConverter.go
@@ -6,30 +6,34 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 )
 
 type Converter struct{}
 
 func (f Converter) ConvertToM3U8(workdirAbsPath string, inputVideoFilename string) (string, error) {
+	log("Generating a hls.sh")
 	scriptPath, err := generateSh(workdirAbsPath, inputVideoFilename)
 	if err != nil {
-		fmt.Println("Couldn't generate FFmpeg sh script")
+		fmt.Println("Couldn't generate hls.sh script")
 		panic(err)
 	}
 
+	log("Making hls.sh an executable")
 	err = makeShExecutable(scriptPath)
 	if err != nil {
-		fmt.Println("Couldn't make FFmpeg sh script executable")
+		fmt.Println("Couldn't make hls.sh script executable")
 		panic(err)
 	}
 
+	log("Executing hls.sh")
 	err = executeSh(scriptPath)
 	if err != nil {
-		fmt.Println("Couldn't execute FFmpeg sh script")
+		fmt.Println("Couldn't execute hls.sh script")
 		panic(err)
 	}
 
-	tryRemovePostProcessFiles(path.Join(workdirAbsPath, inputVideoFilename), path.Join(workdirAbsPath, path.Base(scriptPath)))
+	tryRemovePostProcessFiles(path.Join(workdirAbsPath, changeExtensionToMp4(inputVideoFilename)), path.Join(workdirAbsPath, inputVideoFilename), path.Join(workdirAbsPath, path.Base(scriptPath)))
 
 	playlistPath := path.Join(workdirAbsPath, "out.m3u8")
 
@@ -52,8 +56,16 @@ func generateSh(AbsoluteFolderPath string, inVideoFilename string) (string, erro
 		"\nopenssl rand 16 > " + path.Join(AbsoluteFolderPath, "file.key") +
 		"\necho $BASE_URL/file.key > " + path.Join(AbsoluteFolderPath, "file.keyinfo") +
 		"\necho " + path.Join(AbsoluteFolderPath, "file.key") + " >> " + path.Join(AbsoluteFolderPath, "file.keyinfo") +
-		"\necho $(openssl rand -hex 16) >> " + path.Join(AbsoluteFolderPath, "file.keyinfo") +
-		"\nffmpeg -y -i " + path.Join(AbsoluteFolderPath, inVideoFilename) +
+		"\necho $(openssl rand -hex 16) >> " + path.Join(AbsoluteFolderPath, "file.keyinfo")
+
+	if !strings.HasSuffix(strings.ToLower(inVideoFilename), ".mp4") {
+		log(fmt.Sprintf("%s will be converted to %s", inVideoFilename, changeExtensionToMp4(inVideoFilename)))
+		script += fmt.Sprintf("\nffmpeg -i %s -c:v libx264 -preset ultrafast -c:a aac -b:a 128k %s", path.Join(AbsoluteFolderPath, inVideoFilename), path.Join(AbsoluteFolderPath, changeExtensionToMp4(inVideoFilename)))
+
+		inVideoFilename = changeExtensionToMp4(inVideoFilename)
+		log(fmt.Sprintf("New target file is: %s", inVideoFilename))
+	}
+	script += "\nffmpeg -y -i " + path.Join(AbsoluteFolderPath, inVideoFilename) +
 		" -c:v copy" +
 		" -c:a copy" +
 		" -hls_time 10" +
@@ -61,6 +73,7 @@ func generateSh(AbsoluteFolderPath string, inVideoFilename string) (string, erro
 		" -threads 0" +
 		" -hls_key_info_file " + path.Join(AbsoluteFolderPath, "file.keyinfo") +
 		fmt.Sprintf(" %s", path.Join(AbsoluteFolderPath, "out.m3u8"))
+
 	scriptPath := path.Join(AbsoluteFolderPath, "hls.sh")
 
 	err := os.WriteFile(scriptPath, []byte(script), 0777)
@@ -95,4 +108,12 @@ func executeSh(scriptPath string) error {
 	}
 
 	return nil
+}
+
+func log(message string) {
+	fmt.Printf("[FFmpegConverter]: %s\n", message)
+}
+
+func changeExtensionToMp4(originalPath string) string {
+	return strings.TrimSuffix(originalPath, path.Ext(originalPath)) + ".mp4"
 }
